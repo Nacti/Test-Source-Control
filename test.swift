f88a4c03d6b8dad0ff650161dd0492f4740e72c3
Cédric PETERSEN <ced.petersen@me.com>
2018-06-29 21:42:52 +0200

    Ajout Class TEst

---
+++ test.swift
@@ -0,0 +1,15 @@
+//
+//  test.swift
+//  48 - GitHub
+//
+//  Created by Cédric PETERSEN on 29/06/2018.
+//  Copyright © 2018 Cédric PETERSEN. All rights reserved.
+//
+
+import Foundation
+
+class test {
+    func salut() {
+        print("Salut les Codabeilles")
+    }
+}
